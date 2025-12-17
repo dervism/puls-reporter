05efad6171faae9af8d29045a7a516bd41593dad
dervism <dervis4@gmail.com>
2025-12-17 16:09:28 +0100

    Expand `LetterMap` to include letters K-O.

--- src/main/java/no/dervis/puls/model/filters/Filters.java
+++ src/main/java/no/dervis/puls/model/filters/Filters.java
@@ -8,7 +8,8 @@ import java.util.Set;
 public class Filters {
     public enum LetterMap {
         A(1), B(2), C(3), D(4), E(5),
-        F(6), G(7), H(8), I(9), J(10);
+        F(6), G(7), H(8), I(9), J(10),
+        K(11), L(12), M(13), N(14), O(15);
 
         final int index;
         LetterMap(int i) {
