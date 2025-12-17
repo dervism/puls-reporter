a604d137db98ac2a331096ad5ddd127c29aae48a
dervism <dervis4@gmail.com>
2025-12-18 00:54:37 +0100

    Refactor `PulseRatedQuestionTest`: simplify assertions in min/max tests.

--- src/test/java/no/dervis/puls/model/survey/PulseRatedQuestionTest.java
+++ src/test/java/no/dervis/puls/model/survey/PulseRatedQuestionTest.java
@@ -1,21 +1,20 @@
 package no.dervis.puls.model.survey;
 
-import no.dervis.puls.model.survey.PulseRatedQuestion;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PulseRatedQuestionTest {
 
     @Test
     void has_correct_min() {
-        assertEquals(new PulseRatedQuestion("", List.of(1, 2, 3, 5, 7)).minRating(), 1);
+        assertEquals(1, new PulseRatedQuestion("", List.of(1, 2, 3, 5, 7)).minRating());
     }
 
     @Test
     void has_correct_max() {
-        assertEquals(new PulseRatedQuestion("", List.of(1, 2, 3, 5, 7)).maxRating(), 7);
+        assertEquals(7, new PulseRatedQuestion("", List.of(1, 2, 3, 5, 7)).maxRating());
     }
 }
